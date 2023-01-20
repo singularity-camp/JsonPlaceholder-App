ff0faba113196467ef66d4262adb09cfaa4e7e99
Aslan <assylkhanovaslan@gmail.com>
2023-01-20 18:04:05 +0600

    Added timber

--- app/src/main/java/kz/tutorial/jsonplaceholdertypicode/Tutorial Application.kt
+++ app/src/main/java/kz/tutorial/jsonplaceholdertypicode/Tutorial Application.kt
@@ -1,7 +1,17 @@
 package kz.tutorial.jsonplaceholdertypicode
 
 import android.app.Application
+import timber.log.Timber
 
 class TutorialApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initTimber()
+    }
+
+    private fun initTimber() {
+        Timber.plant(Timber.DebugTree())
+    }
 
 }
