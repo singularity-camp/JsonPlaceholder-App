11973824f95e04ee28670510ddf774e9de913cb3
Aslan Assylkhanov <aslan@MacBook-Pro-Aslan.local>
2023-03-19 15:04:25 +0600

    Glide instructions

--- app/src/main/java/kz/tutorial/jsonplaceholdertypicode/presentation/albums/AlbumsFragment.kt
+++ app/src/main/java/kz/tutorial/jsonplaceholdertypicode/presentation/albums/AlbumsFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import kz.tutorial.jsonplaceholdertypicode.R
 
 class AlbumsFragment : Fragment() {
@@ -15,6 +17,25 @@ class AlbumsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_placeholder, container, false)
+
+        //FAQ: Как пользоваться Glide-ом
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //Вьюшка в которую мы будем загружать изображение
+        //После того как поймете как работать с глайдом, уберите её из этого экрана и код ниже
+        val ivExample: ImageView = view.findViewById(R.id.iv_example)
+
+        //Можете использовать разные весрии Glide.with(). Нажмите внутри скобок Сtrl + P и поймете о чем я
+        //Можно даже саму ImageView передать
+        Glide.with(this)
+                //в load() передаем ссылку нашего изображения
+            .load("https://media.wired.co.uk/photos/606d9b367aff197af7c72a2f/4:3/w_2668,h_2001,c_limit/wired-uk-android-tips-1.jpg")
+                //Указываем наш ImageView в который мы хотим загрузить изображение
+            .into(ivExample)
+        //Всё
     }
 
 }
