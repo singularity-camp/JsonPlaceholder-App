da3e83d989a49167ab01eea6abd42c1591a9240d
Kazybek <kazbek.auez@gmail.com>
2023-03-19 17:38:46 +0600

    Addition of Post Details and Comments of the Post

--- app/src/main/java/kz/tutorial/jsonplaceholdertypicode/domain/PostsRepository.kt
+++ app/src/main/java/kz/tutorial/jsonplaceholdertypicode/domain/PostsRepository.kt
@@ -4,4 +4,7 @@ import kz.tutorial.jsonplaceholdertypicode.domain.Post
 
 interface PostsRepository {
     suspend fun getPosts(): List<Post>
+    suspend fun getPostDetails(id: Int): Post
+
+    suspend fun getComments(postId: Int): List<Comment>
 }
